bb7cdb3744df8e369d16e080447c832492aa2ff7
natnaelh14 <natnaelh14@github.com>
2021-09-17 15:49:01 -0400

    cicd starter code

---
+++ client/src/App.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Restaurant from "./components/Restaurant/Restaurant";
+import NewRestaurantForm from "./components/NewRestaurantForm/NewRestaurantForm";
+
+function App() {
+  const [restaurants, setRestaurants] = useState([]);
+
+  useEffect(() => {
+    getRestaurants();
+  }, []);
+
+  const getRestaurants = () => {
+    axios
+      .get("/api/restaurants")
+      .then((response) => {
+        console.log(response.data);
+        setRestaurants(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-12">
+          <h1 className="text-center">Summer CICD Restaurants</h1>
+        </div>
+      </div>
+      <div className="row" id="restaurants-display">
+        {restaurants.map((restaurant) => (
+          <Restaurant {...restaurant} key={restaurant._id} />
+        ))}
+      </div>
+      <div className="row">
+        <div className="col-sm-2" />
+        <div className="col-sm-8">
+          <h2 className="text-center">Create New Restaurant</h2>
+          <NewRestaurantForm getRestaurants={getRestaurants} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
